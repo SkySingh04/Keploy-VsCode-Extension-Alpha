3e44e08736bd06dc40e7cfc80d1398a04013ea86
Akash <akashsingh2210670@gmail.com>
2024-03-05 00:56:14 +0530

    Fixed script

--- scripts/keploy_record_script.sh
+++ scripts/keploy_record_script.sh
@@ -15,8 +15,8 @@ chmod 666 "$log_file_path"
 echo "Log file path: $log_file_path"
 
 # Execute the keploy record command, redirecting output to the log file
-# /usr/local/bin/keploybin record -c "$command" "$filepath" | tee -a "$log_file_path"
-/usr/local/bin/keploybin record -c "/home/akash/Desktop/samples-go/gin-mongo/test-app-url-shortener" | tee -a "$log_file_path" 
+/usr/local/bin/keploybin record -c "$command" "$filepath" | tee -a "$log_file_path"
+# /usr/local/bin/keploybin record -c "/home/akash/Desktop/samples-go/gin-mongo/test-app-url-shortener" | tee -a "$log_file_path" 
 # todo: remove the above line and uncomment the below line
 
 
