81ae3bd206e0af7d10b1024a535c9ff499857755
Akash Singh <akashsingh2210670@gmail.com>
2024-03-13 06:20:43 +0530

    Finished Test page
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>

--- webviews/components/Main.svelte
+++ webviews/components/Main.svelte
@@ -1,50 +1,117 @@
+
+
 <script>
 </script>
 
 <body>
-  <p class="logo"></p>
-  <pre>
-      ▓██▓▄
-    ▓▓▓▓██▓█▓▄
-      ████████▓▒
-            ▀▓▓███▄                   
-          ▄▌▌▓▓████▄    
-        ▓█████████▌▓▓   
-        ▓▓▓▓▀▀▀▀▓▓▓▓▓▓▌  
-        ▓▌                      
-        ▓
-    </pre>
-  <div id="versionDisplay"></div>
-  <div id="Progress"></div>
-  <div id="filePathDiv"></div>
-  <div id="recordCommandInput">
-    <label for="command">Enter App Command:</label>
-    <input type="text" id="recordCommand" name="command" />
+  <div id="selectFolderDiv">
+    <button id="selectFolderButton" class="secondary"
+      >Select Project Folder</button
+    >
+    <input
+      type="text"
+      id="projectFolder"
+      name="projectFolder"
+      placeholder="Enter Manual Path"
+    />
+  </div>
+  <div id="appCommandDiv">
+    <button id="enterAppCommandButton" disabled="true" class="secondary"
+      >Enter App Command</button
+    >
+    <input
+      type="text"
+      id="recordCommand"
+      name="recordCommand"
+      placeholder="Enter App Command"
+    />
+  </div>
+  <div id="flagsDiv">
+    <div id="flags">
+      <select id="selectflags">
+        <option value="manual">Flag 1</option>
+        <option value="record">Flag 2</option>
+      </select>
+    </div>
+    <div id="flagValue">
+      <select id="selectflagValue">
+        <option value="manual">True</option>
+        <option value="record">False</option>
+      </select>
+    </div>
+  </div>
+  <select id="selectTestCases">
+    <option value="Run all test cases">Run all test cases</option>
+    <option value="Test-Case-1">Test-Case-1</option>
+    <option value="Test-Case-2">Test-Case-2</option>
+  </select>
+  <button id="startTestingButton" disabled="true">Start Testing</button>
+  <hr />
+  <div id="outputDiv">
+    <div id="upperOutputDiv">
+      <img
+        class="keploylogo"
+        src="https://avatars.githubusercontent.com/u/92252339?s=200&v=4"
+        alt="Keploy Logo"
+      />
+      <h3>Command</h3>
+    </div>
+    <div id="recordCommandDiv">
+      <h1 id="generatedRecordCommand">keploy test -c ""</h1>
+    </div>
     <div id="recordedTestCases"></div>
-    <div id="recordingbuttons">
-      <button id="startRecordingButton">Start Recording</button>
-      <button id="stopRecordingButton">Stop Recording</button>
-    </div>
   </div>
-  <div id="testCommandInput">
-    <label for="command">Enter App Command:</label>
-    <input type="text" id="testCommand" name="command" />
-    <div id="TestCases"></div>
-    <div id="testbuttons">
-      <button id="startTestButton">Start Testing</button>
-      <button id="stopTestButton">Stop Testing</button>
-    </div>
-  </div>
-  <div id="utilityButtons">
-    <button id="getVersionButton">Get latest version</button>
-    <button id="updateKeployButton">Update Your Keploy</button>
-    <button id="recordButton">Record Test Cases</button>
-    <button id="testButton">Run Test Cases</button>
-    <div id="updateButtons">
-      <button id="updateKeployBinaryButton">Update Keploy Binary</button>
-      <button id="updateKeployDockerButton">Update Keploy Docker</button>
-    </div>
-    <div id="updateStatus"></div>
-    <div id="recordStatus"></div>
-  </div></body
->
+</body>
+
+<style>
+  #flagsDiv,
+  #upperOutputDiv {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    place-items: center;
+  }
+  .keploylogo {
+    width: 50%;
+  }
+  hr {
+    margin: 0;
+    width: 80%;
+    text-align: center;
+    margin: 20px auto;
+  }
+  #selectTestCases {
+    margin: 20px auto;
+    width: 85%;
+  }
+  #startTestingButton {
+    margin: 20px auto;
+  }
+  button {
+    margin: 10px;
+    width: 75%;
+    border-radius: 5px;
+    border: none;
+  }
+  #selectFolderDiv,
+  #appCommandDiv {
+    display: grid;
+    place-items: center;
+  }
+  #selectFolderDiv {
+    margin: 20px;
+  }
+
+  input {
+    width: 95%;
+    margin: 10px;
+  }
+  #selectflags,
+  #selectflagValue {
+    width: 2fr;
+    margin: auto 0;
+  }
+  #generatedRecordCommand{
+    margin: 0 auto;
+    text-align: center;
+  }
+</style>
